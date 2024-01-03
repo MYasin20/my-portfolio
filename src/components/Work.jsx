840d22da0006f84ad1838ee078061dd1f491e54b
MYasin <yasin.ego10@gmail.com>
2024-01-03 23:26:58 +0800

    fixed spelling of bolgr -> blogr

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -143,12 +143,12 @@ const Work = () => {
               </span>
 
               <div className='pt-8 text-center'>
-                <a href="https://myasin20.github.io/bolgr-landing-page/" target='_blank' rel='noreferrer'>
+                <a href="https://myasin20.github.io/blogr-landing-page/" target='_blank' rel='noreferrer'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/MYasin20/bolgr-landing-page" target='_blank' rel='noreferrer'>
+                <a href="https://github.com/MYasin20/blogr-landing-page" target='_blank' rel='noreferrer'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
